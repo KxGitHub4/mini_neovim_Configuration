14b6937a38e379270b919a0ee3943002410fdfde
keysl1314 <keysl1314@gmail.com>
2025-10-30 09:06:06 -0700

    fix fold fault

---
+++ lsp.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.enable("clangd")
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("pyright")
+			vim.lsp.enable("ts_ls")
+			-- 配置诊断标记符号
+			local signs = {
+				Error = "",
+				Warn = "",
+				Info = "",
+				Hint = "",
+			}
+			-- 定义标记符号 for type, icon in pairs(signs) do vim.fn.sign_define("DiagnosticSign" .. type, {
+			vim.diagnostic.config({
+				virtual_text = {
+					current_line = true,
+				},
+				float = {
+					border = "rounded",
+					source = "always",
+				},
+				signs = true,
+				update_in_insert = false,
+			})
+		end,
+	},
+}
